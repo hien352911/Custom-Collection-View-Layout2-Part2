411dfb8ec9ad8c71f4b9f20fa7a82d13a32bc272
buiduyhien <hien.bd@seesaa.vn>
2020-06-28 18:16:54 +0700

    create CollectionHeaderView and add to collection view

---
+++ Character Collector/CollectionHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionHeaderView.swift
+//  Character Collector
+//
+//  Created by buiduyhien on 6/28/20.
+//  Copyright © 2020 Razeware, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var foregroundImage: UIImageView!
+}
